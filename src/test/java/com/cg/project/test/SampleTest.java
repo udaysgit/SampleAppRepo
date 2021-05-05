357aca54827148dbaf584c3e567e3ee57ee1afce
udaysgit <thondamala.gmail@gmail.com>
2021-05-05 20:58:58 +0530

    internal commit

---
+++ src/test/java/com/cg/project/test/SampleTest.java
@@ -0,0 +1,25 @@
+package com.cg.project.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+	
+	@Test
+    public void test1() {
+        Assert.fail();
+    }
+    @Test
+    public void test2() {
+        Assert.fail();
+    }
+    @Test
+    public void test3() {
+
+    }
+    @Test
+    public void test4() {
+    }
+	
+
+}
